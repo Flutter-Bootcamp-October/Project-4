5b9fc464782d186335f7cfcfe8d8eae7a298f065
Wadha21 <wadha.almuatiri21@gmail.com>
2023-11-02 20:40:02 +0300

    Prodect Details screen

---
+++ wach_store_app/lib/screens/products_details.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:wach_store_app/widgets/row_circle_button_widget.dart';
+
+class ProductDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () {},
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Product Name',
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_bag_outlined, color: Colors.black),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                'lib\\assets\\watches_PNG9866.png',
+                width: double.infinity,
+                height: 350,
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(height: 16),
+              const Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Product Title',
+                        style: TextStyle(
+                          color: Color(0xff163368),
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        '\$9.99',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  RowCircleButton()
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Product Description',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        child: InkWell(
+          onTap: () {},
+          child: Container(
+            width: double.infinity,
+            height: 45,
+            decoration: BoxDecoration(
+              color: const Color(0xfffbcf7a),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.shopping_bag_outlined,
+                  color: Colors.black,
+                ),
+                Text(
+                  '  Add to Cart',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
