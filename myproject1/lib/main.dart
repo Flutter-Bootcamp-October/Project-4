446356d157216e65848b0f6e6b7da822267fb284
senshi900 <dr.fisal99@gmail.com>
2023-11-03 14:39:33 +0300

    adding imges

--- myproject1/lib/main.dart
+++ myproject1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopingpriject/screens/home_screen.dart';
 import 'package:shopingpriject/screens/welcome_screen.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: WelocmeScreen()
+      home: HomeScreen()
          
       
     );
