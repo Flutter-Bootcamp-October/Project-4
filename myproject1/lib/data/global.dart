d77435871bb7ce9deeff0863a1873186a6ba39b5
senshi900 <dr.fisal99@gmail.com>
2023-11-04 16:47:11 +0300

    adding detil screen

--- myproject1/lib/data/global.dart
+++ myproject1/lib/data/global.dart
@@ -1,3 +1,14 @@
 import 'package:shopingpriject/models/product_model.dart';
 
 List<Product> listProucts=[];
+List<Product> orderProuct=[];
+
+double globalPrice = 0;
+
+void calculateGlobalPrice() {
+  globalPrice = 0;
+  for (var element in orderProuct) {
+    globalPrice += element.count! * double.parse(element.price);
+  }
+  print(globalPrice);
+}
--- myproject1/lib/models/product_model.dart
+++ myproject1/lib/models/product_model.dart
@@ -3,7 +3,6 @@ class Product{
   final String name;
   final String description;
   final String price;
-
   int? count;
 
   Product({required this.name, required this.description, required this.price,  required this.image, this.count, });
